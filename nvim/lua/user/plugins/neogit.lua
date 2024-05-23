4f0b1151ada67025ede08b0d16953ff2b4e12864
jockeee <jockeee@duck.com>
2024-05-23 19:42:18 +0200

    nvim

--- nvim/lua/user/plugins/neogit.lua
+++ nvim/lua/user/plugins/neogit.lua
@@ -5,6 +5,7 @@
 -- interactive git interface
 return {
   'NeogitOrg/neogit',
+  tag = 'v0.0.1', -- use tag v0.0.1 if neovim version is < 0.10
   dependencies = {
     'nvim-lua/plenary.nvim', -- required
     'sindrets/diffview.nvim', -- optional - Diff integration
@@ -12,9 +13,6 @@ return {
     -- Only one of these is needed, not both.
     'nvim-telescope/telescope.nvim', -- optional
     -- "ibhagwan/fzf-lua",            -- optional
-  },
-  cmd = { -- Lazy-load on command
-    'Neogit',
   },
   config = true,
 }
