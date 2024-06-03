00c348862a232c40c75c3132c46d6c35eb28dadf
jockeee <jockeee@duck.com>
2024-06-03 19:53:07 +0200

    fish

--- fish/alias.fish
+++ fish/alias.fish
@@ -214,7 +214,7 @@ function upd_npm -d 'npm update'
   end
   if functions -q nvm
     echo -e '\e[1mUpdating npm (for user)\e[0m'
-    echo -e '\e[3mnpm install -g npm@latest\e[0m'
+    echo -e '\e[3mnpm install -g npm@latest\e[0m\n'
     nvm use lts
     npm install -g npm@latest
     echo
